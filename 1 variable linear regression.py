0a848473b01b008b7a006687e0196bec40b97b17
shaongitt <shaongit@gmail.com>
2023-05-15 21:39:11 -0400

    single variable linear regression

---
+++ 1 variable linear regression.py
@@ -0,0 +1,78 @@
+
+# We are inputting sample data in x and the value in ys
+x = [140,155,159,179]
+
+y  = [60,62,67,70]
+w = 0
+b = 0
+alpha = 1.0e-2
+m = len(x)
+iterations=5
+
+gd  = []
+
+
+# impletmenting our Linear Regression 
+
+def model(x,w,b):
+    return w*x+b
+
+
+# cost function 
+
+def cost_func(w,b):
+   cost = 0
+   for idx, i in enumerate(x):
+      cost +=  (model(x[idx],w,b)-y[idx])**2
+
+   cost = cost/(2*m)
+   return cost 
+      
+
+#  you can further optimize  it by combining both function and using one single loop
+
+def update_w(w,b):
+   value = 0
+
+   for idx, i in enumerate(x):
+      value += (model(x[idx],w,b) -y[idx] )* x[idx] 
+
+   return value
+  
+def update_b(w,b):
+   value = 0
+   for idx, i in enumerate(x):
+      value += model(x[idx],w,b) -y[idx] 
+   return value 
+
+
+
+def gradient_descent():
+    
+    global w
+    global b
+    
+    for i in range(iterations):
+   
+      tempw = w
+      w = tempw - alpha*((update_w(tempw,b))/m)
+    
+      tempb = b 
+      b = tempb - alpha *((update_b(tempw,tempb))/m)
+      
+      cost  = cost_func(w,b)
+      gd.append(cost)
+
+
+         
+         
+         
+
+
+
+    
+gradient_descent()
+    
+print(gd[0:10])
+        
+
